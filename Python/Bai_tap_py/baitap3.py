649bd6ef725a2f0e92c64fbfa49d3fb75582e0f2
7D-tfRealC <tranquocdat07042005@gmail.com>
2024-11-22 10:35:26 +0700

    baitappython

---
+++ Python/Bai_tap_py/baitap3.py
@@ -0,0 +1,8 @@
+#chuan hoa chuoi
+a=input("nhap chuoi ky tu: ")
+s1=a.strip()
+s2=s1.lower()
+s3=s2.title()
+s4=s3.split()
+s5=" ".join(s4)
+print(s5)
---
+++ Python/Bai_tap_py/baitap4.py
@@ -0,0 +1,29 @@
+#tinh x3,x4,x5
+"""
+x=double(input("nhap x: "))
+x3=x**3
+x4=x**4
+x5=x**5
+print(f"gia tri cua x3 la:{x3}")
+print(f"gia tri cua x3 la:{x4}")
+print(f"gia tri cua x3 la:{x5}")
+"""
+#chuyen F sang C
+"""
+F=float(input("nhap F: "))
+C=(F-32)/1.8
+print(f"C={C}")
+"""
+#chuan hoa chuoi
+""""
+a=input("nhap ho va ten: ")
+a1=a.count(" ")
+print(f"so tieng đem duoc:{a1} ")
+#tinh dien tich hinh tron va the tich hinh tron
+"""
+import math
+R=float(input("nhap ban kinh : "))
+S=4*math.pi*R**2
+V=(4/3)*math.pi*R**3
+print(f"dien tich hinh tron la:{S} ")
+print(f"dien tich hinh tron la:{V} ")
